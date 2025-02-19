cca96c8505286126630307da684a860d5012ef83
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2025-02-19 09:13:01 +0700

    try to, but it still not working

--- client.cpp
+++ client.cpp
@@ -7,32 +7,38 @@
 
 int main(int argc, char **argv)
 {
-    int length, sockMain;
+    int length, sockMain, numb;
     struct sockaddr_in serv, client;
     sockMain = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockMain < 0)
     {
-        std::cerr << "cant open" << '\n';
+        std::cerr << "cant open socket" << '\n';
         return 1;
     }
+    numb = atoi(argv[2]);
     socklen_t size = sizeof(struct sockaddr);
     serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = 0;
-    if (bind(sockMain, (struct sockaddr *) &serv, sizeof(serv)))
+    serv.sin_addr.s_addr = INADDR_ANY;
+    serv.sin_port = htons(atoi(argv[1]));
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = INADDR_ANY;
+    client.sin_port = 0;
+    int cnct = bind(sockMain, (struct sockaddr *) &client, sizeof(client));
+    if (cnct == -1)
     {
         std::cerr << "cant bind" << '\n';
         return 2;
     }
-    if (getsockname(sockMain, (struct sockaddr *) &serv, &size))
+    if (getsockname(sockMain, (struct sockaddr *) &client, &size))
     {
         std::cerr << "cant get sock name" << '\n';
         return 3;
     }
-    std::cout << "server port is: " << ntohs(serv.sin_port) << '\n';
+    std::cout << "my port is: " << ntohs(client.sin_port) << '\n';
     while (1)
     {
-        //recivefrom();
+        int tmp = sendto(cnct, &numb, 4, 0, (struct sockaddr*) &client, size);
+        std::cout << ntohs(serv.sin_port) << " " << ntohs(client.sin_port) << " " << numb << '\n';
     }
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -3,23 +3,27 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 
 int main(int argc, char **argv)
 {
-    int length, sockMain;
+    int length, sockMain, cnct, buf;
     struct sockaddr_in serv, client;
     sockMain = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockMain < 0)
     {
-        std::cerr << "cant open" << '\n';
+        std::cerr << "cant open socket" << '\n';
         return 1;
     }
+    buf = 0;
     socklen_t size = sizeof(struct sockaddr);
     serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv.sin_addr.s_addr = INADDR_ANY;
     serv.sin_port = 0;
-    if (bind(sockMain, (struct sockaddr *) &serv, sizeof(serv)))
+    client.sin_addr.s_addr = AF_INET;
+    client.sin_family =INADDR_ANY;
+    client.sin_port = 0;
+    cnct = bind(sockMain, (struct sockaddr *) &serv, sizeof(serv));
+    if (cnct == -1)
     {
         std::cerr << "cant bind" << '\n';
         return 2;
@@ -32,7 +36,8 @@ int main(int argc, char **argv)
     std::cout << "server port is: " << ntohs(serv.sin_port) << '\n';
     while (1)
     {
-        //recivefrom();
+        int tmp = recvfrom(cnct, &buf, 4, 0, (struct sockaddr *) &client, &size);
+        std::cout << ntohs(serv.sin_port) << " " << buf << '\n';
     }
     return 0;
 }
